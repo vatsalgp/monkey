8eee6fc499f0f143d2a46ac304ebe8706b4c44a5
Vatsal Gupta <hi@vatsalgp.com>
2026-04-05 18:22:22 +0530

    Added basic token types

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/vatsalgp/monkey/token"
+)
 
 func main() {
 	fmt.Println("Hello from Monkey!")
+	four := token.Token{Type: token.INTEGER_LITERAL, Literal: "4"}
+	fmt.Println(four)
 }
