3175804239a51c5e08e9c8f918d83814808ebf81
Vatsal Gupta <hi@vatsalgp.com>
2026-04-05 20:43:01 +0530

    Removed extra dep

--- token/token.go
+++ token/token.go
@@ -1,16 +1,12 @@
 package token
 
-import (
-	"fmt"
-)
-
 type Token struct {
 	Type    TokenType
 	Literal TokenLiteral
 }
 
 func (token Token) String() string {
-	return fmt.Sprintf("Token(%s, '%s')", token.Type, token.Literal)
+	return "Token(" + string(token.Type) + ", '" + string(token.Literal) + "')"
 }
 
 func CreateTokenString(word string) string {
