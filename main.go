ca9c3c4b11ac3c4de7baf0d1ce5dd25213d9a4b4
Vatsal Gupta <hi@vatsalgp.com>
2026-04-05 20:20:42 +0530

    Added support for String Literal

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/vatsalgp/monkey/token"
 )
 
+func readAndProcessToken(reader *bufio.Reader) {
+	fmt.Print("Enter your token: ")
+
+	line, _ := reader.ReadString('\n')
+	word := strings.TrimSpace(line)
+
+	token := token.CreateToken(word)
+	fmt.Printf("Word '%s' is %s\n\n", word, token)
+}
+
 func main() {
 	fmt.Printf("Hello from Monkey!\n\n")
 
+	reader := bufio.NewReader(os.Stdin)
+
 	for {
-		var s string
-		fmt.Print("Enter your token: ")
-		fmt.Scan(&s)
-		fmt.Printf("String %s is %s\n\n", s, token.CreateToken(s))
+		readAndProcessToken(reader)
 	}
 }
--- token/token.go
+++ token/token.go
@@ -11,200 +11,200 @@ type Token struct {
 }
 
 func (token Token) String() string {
-	return fmt.Sprintf("Token(%s, %s)", token.Type, token.Literal)
+	return fmt.Sprintf("Token(%s, '%s')", token.Type, token.Literal)
 }
 
-func CreateToken(s string) Token {
-	if s == "" {
+func CreateToken(word string) Token {
+	if word == "" {
 		return Token{
 			Type:    END_OF_FILE,
 			Literal: TokenLiteral(END_OF_FILE),
 		}
 	}
 
-	if s == "let" {
+	if word == "let" {
 		return Token{
 			Type:    LET,
 			Literal: TokenLiteral(LET),
 		}
 	}
 
-	if s == "fn" {
+	if word == "fn" {
 		return Token{
 			Type:    FUNCTION,
 			Literal: TokenLiteral(FUNCTION),
 		}
 	}
 
-	if s == "return" {
+	if word == "return" {
 		return Token{
 			Type:    RETURN,
 			Literal: TokenLiteral(RETURN),
 		}
 	}
 
-	if s == "+" {
+	if word == "+" {
 		return Token{
 			Type:    PLUS,
 			Literal: TokenLiteral(PLUS),
 		}
 	}
 
-	if s == "-" {
+	if word == "-" {
 		return Token{
 			Type:    MINUS,
 			Literal: TokenLiteral(MINUS),
 		}
 	}
 
-	if s == "*" {
+	if word == "*" {
 		return Token{
 			Type:    MULTIPLY,
 			Literal: TokenLiteral(MULTIPLY),
 		}
 	}
 
-	if s == "/" {
+	if word == "/" {
 		return Token{
 			Type:    DIVIDE,
 			Literal: TokenLiteral(DIVIDE),
 		}
 	}
 
-	if s == "=" {
+	if word == "=" {
 		return Token{
 			Type:    ASSIGN,
 			Literal: TokenLiteral(ASSIGN),
 		}
 	}
 
-	if s == "==" {
+	if word == "==" {
 		return Token{
 			Type:    IS_EQUAL,
 			Literal: TokenLiteral(IS_EQUAL),
 		}
 	}
 
-	if s == "!=" {
+	if word == "!=" {
 		return Token{
 			Type:    IS_NOT_EQUAL,
 			Literal: TokenLiteral(IS_NOT_EQUAL),
 		}
 	}
 
-	if s == "<" {
+	if word == "<" {
 		return Token{
 			Type:    IS_LESS_THAN,
 			Literal: TokenLiteral(IS_LESS_THAN),
 		}
 	}
 
-	if s == ">" {
+	if word == ">" {
 		return Token{
 			Type:    IS_GREATER_THAN,
 			Literal: TokenLiteral(IS_GREATER_THAN),
 		}
 	}
 
-	if s == "<=" {
+	if word == "<=" {
 		return Token{
 			Type:    IS_LESS_THAN_OR_EQUAL,
 			Literal: TokenLiteral(IS_LESS_THAN_OR_EQUAL),
 		}
 	}
 
-	if s == ">=" {
+	if word == ">=" {
 		return Token{
 			Type:    IS_GREATER_THAN_OR_EQUAL,
 			Literal: TokenLiteral(IS_GREATER_THAN_OR_EQUAL),
 		}
 	}
 
-	if s == "," {
+	if word == "," {
 		return Token{
 			Type:    COMMA,
 			Literal: TokenLiteral(COMMA),
 		}
 	}
 
-	if s == ";" {
+	if word == ";" {
 		return Token{
 			Type:    SEMICOLON,
 			Literal: TokenLiteral(SEMICOLON),
 		}
 	}
 
-	if s == "(" {
+	if word == "(" {
 		return Token{
 			Type:    LEFT_PAREN,
 			Literal: TokenLiteral(LEFT_PAREN),
 		}
 	}
 
-	if s == ")" {
+	if word == ")" {
 		return Token{
 			Type:    RIGHT_PAREN,
 			Literal: TokenLiteral(RIGHT_PAREN),
 		}
 	}
 
-	if s == "{" {
+	if word == "{" {
 		return Token{
 			Type:    LEFT_BRACE,
 			Literal: TokenLiteral(LEFT_BRACE),
 		}
 	}
 
-	if s == "}" {
+	if word == "}" {
 		return Token{
 			Type:    RIGHT_BRACE,
 			Literal: TokenLiteral(RIGHT_BRACE),
 		}
 	}
 
-	if s == "true" {
+	if word == "true" {
 		return Token{
 			Type:    BOOLEAN_LITERAL,
 			Literal: TokenLiteral("true"),
 		}
 	}
 
-	if s == "false" {
+	if word == "false" {
 		return Token{
 			Type:    BOOLEAN_LITERAL,
 			Literal: TokenLiteral("false"),
 		}
 	}
 
-	isInteger, _ := regexp.MatchString(`^\d+$`, s)
+	isInteger, _ := regexp.MatchString(`^\d+$`, word)
 	if isInteger {
 		return Token{
 			Type:    INTEGER_LITERAL,
-			Literal: TokenLiteral(s),
+			Literal: TokenLiteral(word),
 		}
 	}
 
-	isFloat, _ := regexp.MatchString(`^\d+\.\d+$`, s)
+	isFloat, _ := regexp.MatchString(`^\d+\.\d+$`, word)
 	if isFloat {
 		return Token{
 			Type:    FLOAT_LITERAL,
-			Literal: TokenLiteral(s),
+			Literal: TokenLiteral(word),
 		}
 	}
 
-	isString, _ := regexp.MatchString(`^\".*\"$`, s)
+	isString, _ := regexp.MatchString(`^\".*\"$`, word)
 	if isString {
 		return Token{
 			Type:    STRING_LITERAL,
-			Literal: TokenLiteral(s),
+			Literal: TokenLiteral(word),
 		}
 	}
 
-	isIdentifier, _ := regexp.MatchString(`^[a-zA-Z_]+\w*$`, s)
+	isIdentifier, _ := regexp.MatchString(`^[a-zA-Z_]+\w*$`, word)
 	if isIdentifier {
 		return Token{
 			Type:    IDENTIFIER,
-			Literal: TokenLiteral(s),
+			Literal: TokenLiteral(word),
 		}
 	}
 
