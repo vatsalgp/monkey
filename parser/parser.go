d18e7b3dad2281f8ea5f09f6156211fea1e9fb82
Vatsal Gupta <hi@vatsalgp.com>
2026-04-14 20:44:23 +0530

    Move parse funcs to own file

--- parser/parser.go
+++ parser/parser.go
@@ -3,7 +3,6 @@ package parser
 import (
 	"fmt"
 
-	"github.com/vatsalgp/monkey/ast"
 	"github.com/vatsalgp/monkey/lexer"
 	"github.com/vatsalgp/monkey/token"
 )
@@ -34,7 +33,7 @@ func (p *Parser) peekTokType() token.Type {
 	return p.peekTok.Type
 }
 
-func (p *Parser) expectCurrTokType(tokType token.Type) bool {
+func (p *Parser) expectTokType(tokType token.Type) bool {
 	if p.currTokType() == tokType {
 		return true
 	}
@@ -52,131 +51,4 @@ func (p *Parser) logError(msg string) {
 
 func (p *Parser) logTypeError(tok token.Type) {
 	p.logError(fmt.Sprintf("expected token to be %s, got %s instead", tok, p.currTokType()))
-}
-
-// TODO: Handle errors: Empty token or incorrect token
-
-func (p *Parser) ParseProgram() *ast.Program {
-	program := &ast.Program{}
-	program.Statements = []ast.Statement{}
-
-	for p.currTokType() != token.END_OF_FILE.Type {
-		stmt := p.parseStatement()
-		if stmt == nil {
-			// p.logError("Expected Statement"); // Needed?
-			break
-		}
-		program.Statements = append(program.Statements, stmt)
-	}
-
-	return program
-}
-
-func (p *Parser) parseStatement() ast.Statement {
-	switch p.currTokType() {
-	case token.LET.Type:
-		return p.parseLetStmt()
-	default:
-		p.logError("Expected Valid Statement")
-		return nil
-	}
-}
-
-func (p *Parser) parseLetToken() *token.Token {
-	if !p.expectCurrTokType(token.LET.Type) {
-		return nil
-	}
-	tok := p.currTok
-	p.advanceToken()
-	return tok
-}
-
-func (p *Parser) parseIdentifier() *ast.Identifier {
-	if !p.expectCurrTokType(token.IDENTIFIER.Type) {
-		return nil
-	}
-	iden := &ast.Identifier{Token: p.currTok, Value: p.currTok.Literal}
-	p.advanceToken()
-	return iden
-}
-
-func (p *Parser) parseAssign() bool {
-	if !p.expectCurrTokType(token.ASSIGN.Type) {
-		return false
-	}
-	p.advanceToken()
-	return true
-}
-
-func (p *Parser) parseSemi() bool {
-	if !p.expectCurrTokType(token.SEMICOLON.Type) {
-		return false
-	}
-	p.advanceToken()
-	return true
-}
-
-func (p *Parser) parseLetStmt() *ast.LetStatement {
-	letStmt := &ast.LetStatement{}
-
-	// let
-	letToken := p.parseLetToken()
-	if letToken == nil {
-		return nil
-	}
-	letStmt.Token = letToken
-
-	// x
-	iden := p.parseIdentifier()
-	if iden == nil {
-		return nil
-	}
-	letStmt.Name = iden
-
-	// =
-	isAssign := p.parseAssign()
-	if !isAssign {
-		return nil
-	}
-
-	// (y)
-	expr := p.parseExpression()
-	// TODO: Do Expr error handling when expr code is done
-	// if expr == nil {
-	// 	return nil
-	// }
-	letStmt.Value = expr
-
-	return letStmt
-}
-
-func (p *Parser) parseExpression() ast.Expression {
-	// TODO: Parse Literal
-
-	if p.currTokType() == token.IDENTIFIER.Type && p.peekTokType() == token.SEMICOLON.Type {
-		iden := p.parseIdentifier()
-		if iden == nil {
-			return nil
-		}
-
-		// ;
-		isSemi := p.parseSemi()
-		if !isSemi {
-			return nil
-		}
-
-		return iden
-	}
-
-	for p.currTokType() != token.SEMICOLON.Type {
-		p.advanceToken()
-	}
-
-	// ;
-	isSemi := p.parseSemi()
-	if !isSemi {
-		return nil
-	}
-
-	return nil
 }
